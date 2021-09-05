b99c3c55ac70d32380f3d6f4d114f623a60d19f9
seonghbae <fullbloomh3ck@gmail.com>
2021-09-06 04:31:20 +0900

    이중우선순위큐 풀이

--- programmers/Heap/doubleEndedPriorityQueue/doubleEndedPrioirtyQueue.java
+++ programmers/Heap/doubleEndedPriorityQueue/doubleEndedPrioirtyQueue.java
@@ -1,1 +1,15 @@
 package programmers.Heap.doubleEndedPriorityQueue;
+import java.util.TreeMap;
+
+class Solution {
+    public int[] solution(String[] operations) {
+        TreeMap<Integer, Integer> dpq = new TreeMap<Integer, Integer>();
+        for(int i=0; i<operations.length; i++){
+            if(operations[i].charAt(0)=='I') dpq.put(Integer.parseInt(operations[i].split(" ")[1]), 0);
+            else if(operations[i].charAt(2)=='1') dpq.pollLastEntry();
+            else dpq.pollFirstEntry();
+        }
+        if(dpq.isEmpty()) return new int[] {0, 0};
+        return new int[] {dpq.lastKey(), dpq.firstKey()};
+    }
+}
---
+++ programmers/Heap/doubleEndedPriorityQueue/readme.md
@@ -0,0 +1,10 @@
+# 최종 설계
+- 이중우선순위큐를 구현해야 하므로 `TreeMap`의 메서드를 활용합니다.
+    - 기본적으로 정렬
+    - 시작과 끝에 접근 가능한 메서드 존재
+
+# 최종 결과
+![최종 결과](https://user-images.githubusercontent.com/59535609/132139085-e4e67b7c-a87c-4cc4-a0c8-162ec4b11180.jpg)
+
+# 문제 출처
+https://programmers.co.kr/learn/courses/30/lessons/42628
